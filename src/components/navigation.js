c7303f2242db51a4b2d9ceddba118fe780dcf2d2
Menemi <danila.titov1210@yandex.ru>
2023-10-15 21:46:01 +0300

    fix: hotfixes

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -5,7 +5,7 @@ export default function Navigation() {
   const navItems = [
     {
       name: "leaderboard",
-      path: "/leaderboard",
+      path: "/",
     },
     {
       name: "global top",
--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ import Navigation from "./components/navigation";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import GlobalTop from "./pages/global-top";
 import Diagrams from "./pages/diagrams";
-import Leaderboard from "./pages/leaderboard";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Router>
     <Navigation/>
     <Routes>
-      <Route path="/leaderboard" element={<Leaderboard/>}/>
+      <Route path="/" element={<App/>}/>
       <Route path="/global-top" element={<GlobalTop/>}/>
       <Route path="/diagrams" element={<Diagrams/>}/>
     </Routes>
