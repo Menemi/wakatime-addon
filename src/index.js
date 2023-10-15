ad4e33aac30b9d0e087a4dd30d6ff44b0a86fece
Menemi <danila.titov1210@yandex.ru>
2023-10-15 21:52:26 +0300

    fix: hotfixes

--- src/index.js
+++ src/index.js
@@ -1,20 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Navigation from "./components/navigation";
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import GlobalTop from "./pages/global-top";
-import Diagrams from "./pages/diagrams";
+import {HashRouter as Router} from "react-router-dom";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Router>
-    <Navigation/>
-    <Routes>
-      <Route path="/" element={<App/>}/>
-      <Route path="/global-top" element={<GlobalTop/>}/>
-      <Route path="/diagrams" element={<Diagrams/>}/>
-    </Routes>
+    <App/>
   </Router>
 );
